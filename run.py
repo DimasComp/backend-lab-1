2864b5ff08bbcfd8b645ec70ad048026d2e13add
DimasComp <d.demchyk.im21@kpi.ua>
2025-01-11 23:20:33 +0200

    Add modifiable host and default to 0.0.0.0

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from server import app
 import os
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv('PORT', 5000))
+    app.run(debug=True, port=os.getenv('PORT', 5000), host=os.getenv('HOST', '0.0.0.0'))
