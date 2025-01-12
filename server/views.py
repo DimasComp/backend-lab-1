2c634abb236a064f005213d105e3e71c9843451d
DimasComp <d.demchyk.im21@kpi.ua>
2025-01-12 13:28:30 +0200

    Make healthcheck return time and not only status

--- server/views.py
+++ server/views.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, jsonify
+from datetime import datetime
+import pytz
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/healthcheck')
 def healthcheck():
-    return jsonify({'status': 'ok'})
+    timezone = pytz.timezone('Etc/GMT-2')
+    current_time = datetime.now(timezone)
+    return jsonify({'status': 'ok', 'datetime': current_time.strftime('%d-%m-%Y %H:%M:%S')})
